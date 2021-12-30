ed0f9c0d45c2020771716dc4d67c9388aee9e6df
Rahul Rathore <rathorerahul355@gmail.com>
2021-12-30 13:28:22 +0530

    Updates

---
+++ precache-manifest.84688a30d234cd949a3466f695dab7c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2401514f30f53a014442",
+    "url": "/Text-Utils/static/css/main.0c2f469a.chunk.css"
+  },
+  {
+    "revision": "2401514f30f53a014442",
+    "url": "/Text-Utils/static/js/main.2401514f.chunk.js"
+  },
+  {
+    "revision": "b30741ea70ee088bcae7",
+    "url": "/Text-Utils/static/js/1.b30741ea.chunk.js"
+  },
+  {
+    "revision": "ee8091b65c82821b2159",
+    "url": "/Text-Utils/static/js/2.ee8091b6.chunk.js"
+  },
+  {
+    "revision": "47366c16f1f3eb521fd9",
+    "url": "/Text-Utils/static/js/runtime~main.47366c16.js"
+  },
+  {
+    "revision": "e8e40cf7f05179f91c751b0ae314432d",
+    "url": "/Text-Utils/index.html"
+  }
+];
